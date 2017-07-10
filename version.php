2c816bb94d6396c2672368de6fc51f866db0ae1b
Gordon Bateson <gordonbateson@gmail.com>
2017-07-10 09:22:23 +0900

    datafield_admin fetch and set default values from additional info user fields

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'datafield_admin';
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2017070926;
+$plugin->version   = 2017071027;
 $plugin->release   = '2017-07-10 (27)';
