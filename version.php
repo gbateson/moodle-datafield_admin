be1155f28a5d3eae69cef3d14ef69581f6042462
Gordon Bateson <gordonbateson@gmail.com>
2025-02-02 22:15:57 +0900

    datafield_template add files for mustache templates

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'datafield_admin';
 $plugin->requires  = 2012062500; // Moodle 2.3
-$plugin->version   = 2025020210;
-$plugin->release   = '2025-02-02 (10)';
+$plugin->version   = 2025020211;
+$plugin->release   = '2025-02-02 (11)';
